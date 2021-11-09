71195c68692453a4d32084d0531b3d46be053ec5
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-09 19:08:28 +0530

    Distance added

---
+++ Distance.java
@@ -0,0 +1,23 @@
+package com.functional_program;
+
+import java.util.Scanner;
+
+public class Distance {
+
+	public void distance() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print(" Please enter the x coordinate: ");
+		int x = scan.nextInt();
+		
+		System.out.print(" Please enter the y coordinate: ");
+		int y = scan.nextInt();
+		
+		double length = Math.sqrt( Math.pow(x, 2) + Math.pow(y, 2));
+		
+		System.out.println(" The distance of the point from the origin is: " + length);
+		
+		scan.close();
+	}
+}
+
