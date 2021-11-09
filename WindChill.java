36a5d9560eb52c4758cc1e46c01ec710bd600189
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-09 19:31:16 +0530

    Wind chill added

---
+++ WindChill.java
@@ -0,0 +1,31 @@
+package com.functional_program;
+
+import java.util.Scanner;
+
+public class WindChill {
+
+	public void solution() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print(" Please enter the value of t(F): ");
+		double t = scan.nextDouble();
+		if(t > 50) {
+			System.out.println(" Please enter a temp value that is less than 50");
+			scan.close();
+			return;
+		}
+		System.out.print(" Please enter the value of v(Mph): ");
+		
+		double v = scan.nextDouble();
+		if(v > 120 || v < 3) {
+			System.out.println(" Please enter a velocity value that is between 3 and 120");
+			scan.close();
+			return;
+		}
+		double wind = 35.74 + 0.6215*t + (0.4275*t - 35.75)*Math.pow(v, 0.16);
+		
+		System.out.println(" The wind speed is: " + wind);
+		scan.close();
+	}
+}
+
