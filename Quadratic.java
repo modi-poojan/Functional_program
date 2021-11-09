58bb591609d731163accc4cacc40c2b8205ade75
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-09 19:18:00 +0530

    Quadatratic added

---
+++ Quadratic.java
@@ -0,0 +1,26 @@
+package com.functional_program;
+
+import java.util.Scanner;
+
+public class Quadratic {
+
+	public void quadratic() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print(" Please enter the value of a: ");
+		int a = scan.nextInt();
+		
+		System.out.print(" Please enter the value of b: ");
+		int b = scan.nextInt();
+		
+		System.out.print(" Please enter the value of c: ");
+		int c = scan.nextInt();
+		
+		int delta = b*b - 4*a*c;
+		double x1 = (-b + Math.sqrt(delta)) / 2*a;
+		double x2 = (-b - Math.sqrt(delta)) / 2*a;
+		
+		System.out.println(" The two roots are: " + x1 + " and " + x2);
+		scan.close();
+	}
+}
